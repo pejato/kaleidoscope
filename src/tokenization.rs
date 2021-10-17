99ce36526c365ea3fe110dbeb9b0f40e9def5125
Peter Tolsma <pejato@umich.edu>
2021-10-17 14:06:33 -0500

    More tokenization tests, change get_token to take a reader argument

--- src/tokenization.rs
+++ src/tokenization.rs
@@ -1,5 +1,3 @@
-use crate::test_utilities::approx_equal;
-
 use std::{io::Read, string::String};
 
 // TODO: Operators?
@@ -20,14 +18,16 @@ fn is_newline(c: u8) -> bool {
     c == ('\n' as u8) || c == ('\r' as u8)
 }
 
-// TODO: Look at actual error type in stdin().read
-fn get_token() -> Token {
+// TODO: Look at actual error type in reader.read
+fn get_token<T>(mut reader: T) -> Token
+where
+    T: Read,
+{
     let mut char: [u8; 1] = [0];
-    let mut stdin = std::io::stdin();
     let mut n: usize;
 
     loop {
-        n = stdin.read(&mut char).unwrap();
+        n = reader.read(&mut char).unwrap();
         if n == 0 {
             break;
         }
@@ -38,13 +38,13 @@ fn get_token() -> Token {
 
     // Def, Extern, or Identifier
     if char[0].is_ascii_alphabetic() {
-        return tok_def_extern_or_ident(char, stdin);
+        return tok_def_extern_or_ident(char, reader);
         // Number
     } else if char[0].is_ascii_digit() || is_decimal(char[0]) {
-        return tok_number(char, stdin);
+        return tok_number(char, reader);
         // Comment
     } else if char[0] == ('#' as u8) {
-        return tok_comment(char, stdin);
+        return tok_comment(char, reader);
     }
 
     if n == 0 {
@@ -54,7 +54,7 @@ fn get_token() -> Token {
     }
 }
 
-fn tok_number<T>(mut char: [u8; 1], mut stdin: T) -> Token
+fn tok_number<T>(mut char: [u8; 1], mut reader: T) -> Token
 where
     T: Read,
 {
@@ -62,7 +62,7 @@ where
     let mut num_string = (char[0] as char).to_string();
 
     loop {
-        let n = stdin.read(&mut char).unwrap();
+        let n = reader.read(&mut char).unwrap();
         if n == 0 {
             break;
         }
@@ -83,37 +83,37 @@ where
     return Token::Number(num_val);
 }
 
-fn tok_comment<T>(mut char: [u8; 1], mut stdin: T) -> Token
+fn tok_comment<T>(mut char: [u8; 1], mut reader: T) -> Token
 where
     T: Read,
 {
     loop {
         // Read until EOF or a newline character
-        let n = stdin.read(&mut char).unwrap();
+        let n = reader.read(&mut char).unwrap();
         if n == 0 {
             return Token::EOF;
         }
         if is_newline(char[0]) {
             // Strip characters until we encounter a non-newline
             while is_newline(char[0]) {
-                match stdin.read(&mut char) {
+                match reader.read(&mut char) {
                     Err(_) | Ok(0) => return Token::EOF,
                     _ => (),
                 }
             }
-            return get_token();
+            return get_token(reader);
         }
     }
 }
 
-fn tok_def_extern_or_ident<T>(mut char: [u8; 1], mut stdin: T) -> Token
+fn tok_def_extern_or_ident<T>(mut char: [u8; 1], mut reader: T) -> Token
 where
     T: Read,
 {
     let mut ident = (char[0] as char).to_string();
 
     while char[0].is_ascii_alphanumeric() {
-        let n = stdin.read(&mut char).unwrap();
+        let n = reader.read(&mut char).unwrap();
         if n == 0 {
             break;
         }
@@ -128,12 +128,16 @@ where
 }
 #[cfg(test)]
 mod tests {
+    use crate::test_utilities::approx_equal;
+
     use super::*;
 
     #[test]
     fn test_tok_number_valid_integer() {
-        let input = "23456789".as_bytes();
-        let buf: [u8; 1] = ['1' as u8];
+        let mut buf: [u8; 1] = [0];
+        let mut input = "123456789".as_bytes();
+        let _ = input.read(&mut buf);
+
         let result = tok_number(buf, input);
 
         match result {
@@ -144,9 +148,105 @@ mod tests {
 
     #[test]
     fn test_tok_number_valid_decimal() {
-        let input = "23456789.3798901".as_bytes();
-        let buf: [u8; 1] = ['1' as u8];
+        let mut buf: [u8; 1] = [0];
+        let mut input = "123456789.3798901".as_bytes();
+        let _ = input.read(&mut buf);
+
         let result = tok_number(buf, input);
+
+        match result {
+            Token::Number(n) => assert!(crate::test_utilities::approx_equal(
+                n,
+                123456789.3798901,
+                15
+            )),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "called `Result::unwrap()` on an `Err` value: ParseFloatError { kind: Invalid }"
+    )]
+    fn test_tok_number_too_many_decimal_points() {
+        let mut buf: [u8; 1] = [0];
+        let mut input = "123456789.37989.01".as_bytes();
+        let _ = input.read(&mut buf);
+
+        let _ = tok_number(buf, input);
+    }
+
+    #[test]
+    fn test_tok_valid_def() {
+        let mut buf: [u8; 1] = [0];
+        let mut input = "def".as_bytes();
+        let _ = input.read(&mut buf);
+
+        let result = tok_def_extern_or_ident(buf, input);
+
+        match result {
+            Token::Def => (),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_tok_valid_extern() {
+        let mut buf: [u8; 1] = [0];
+        let mut input = "extern".as_bytes();
+        let _ = input.read(&mut buf);
+
+        let result = tok_def_extern_or_ident(buf, input);
+
+        match result {
+            Token::Extern => (),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_tok_comment_with_newline_then_eof() {
+        let mut buf: [u8; 1] = [0];
+        let mut input = "# Some text like def extern\n".as_bytes();
+        let _ = input.read(&mut buf);
+
+        let result = tok_comment(buf, input);
+
+        match result {
+            Token::EOF => (),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_tok_comment_with_no_newline_then_eof() {
+        let mut buf: [u8; 1] = [0];
+        let mut input = "# Some text like def extern".as_bytes();
+        let _ = input.read(&mut buf);
+
+        let result = tok_comment(buf, input);
+
+        match result {
+            Token::EOF => (),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_token_valid_integer() {
+        let input = "123456789".as_bytes();
+        let result = get_token(input);
+
+        match result {
+            Token::Number(n) => assert!(approx_equal(n, 123456789.0, 15)),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_token_valid_decimal() {
+        let input = "123456789.3798901".as_bytes();
+        let result = get_token(input);
 
         match result {
             Token::Number(n) => assert!(approx_equal(n, 123456789.3798901, 15)),
@@ -158,20 +258,51 @@ mod tests {
     #[should_panic(
         expected = "called `Result::unwrap()` on an `Err` value: ParseFloatError { kind: Invalid }"
     )]
-    fn test_tok_number_too_many_decimal_points() {
-        let input = "23456789.37989.01".as_bytes();
-        let buf: [u8; 1] = ['1' as u8];
-        let result = tok_number(buf, input);
+    fn test_get_token_too_many_decimal_points() {
+        let input = "123456789.37989.01".as_bytes();
+        let _ = get_token(input);
     }
 
     #[test]
-    fn test_tok_valid_def() {
-        let input = "ef".as_bytes();
-        let buf: [u8; 1] = ['d' as u8];
-        let result = tok_def_extern_or_ident(buf, input);
+    fn test_get_token_def() {
+        let input = "def".as_bytes();
+        let result = get_token(input);
 
         match result {
             Token::Def => (),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_token_extern() {
+        let input = "extern".as_bytes();
+        let result = get_token(input);
+
+        match result {
+            Token::Extern => (),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_token_with_comment_newline_then_eof() {
+        let input = "# Some text like def extern\n".as_bytes();
+        let result = get_token(input);
+
+        match result {
+            Token::EOF => (),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_token_with_comment_no_newline_then_eof() {
+        let input = "# Some text like def extern".as_bytes();
+        let result = get_token(input);
+
+        match result {
+            Token::EOF => (),
             _ => assert!(false),
         }
     }
