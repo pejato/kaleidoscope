5b0e54e03e11ee7fdc7009ba32e24d7f00e0e321
Peter Tolsma <pejato@umich.edu>
2021-11-20 17:33:57 -0600

    Use parameterized Write objects for driver output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::parser::Parser;
-use std::io::{stdin, BufRead};
+use std::io::{stdin, stdout, BufRead, Write};
 use tokenization::{Lexer, Token};
 
 pub mod ast;
@@ -8,25 +8,34 @@ pub mod parser;
 mod test_utilities;
 pub mod tokenization;
 
-fn handle_function_definition<T: BufRead>(parser: &mut Parser, lexer: &mut Lexer<T>) {
+// TODO: Refactor these into a Driver struct or something
+fn handle_function_definition<T: BufRead>(
+    parser: &mut Parser,
+    lexer: &mut Lexer<T>,
+    output: &mut dyn Write,
+) {
     if parser.parse_function_definition(lexer).is_some() {
-        eprintln!("Parsed a function definition");
+        write!(output, "Parsed a function definition").unwrap();
     } else {
         lexer.get_next_token();
     }
 }
 
-fn handle_extern<T: BufRead>(parser: &mut Parser, lexer: &mut Lexer<T>) {
+fn handle_extern<T: BufRead>(parser: &mut Parser, lexer: &mut Lexer<T>, output: &mut dyn Write) {
     if parser.parse_extern(lexer).is_some() {
-        eprintln!("Parsed an extern");
+        write!(output, "Parsed an extern").unwrap();
     } else {
         lexer.get_next_token();
     }
 }
 
-fn handle_top_level_expression<T: BufRead>(parser: &mut Parser, lexer: &mut Lexer<T>) {
+fn handle_top_level_expression<T: BufRead>(
+    parser: &mut Parser,
+    lexer: &mut Lexer<T>,
+    output: &mut dyn Write,
+) {
     if parser.parse_top_level_expression(lexer).is_some() {
-        eprintln!("Parsed a top level expression");
+        write!(output, "Parsed a top level expression").unwrap();
     } else {
         lexer.get_next_token();
     }
@@ -35,18 +44,22 @@ fn handle_top_level_expression<T: BufRead>(parser: &mut Parser, lexer: &mut Lexe
 fn main() {
     let mut parser = Parser::new();
     let stdin = stdin();
-    let instream_handle = stdin.lock();
-    let mut lexer = Lexer::new(instream_handle);
+    let stdout = stdout();
+
+    let mut outstream_handle = stdout.lock();
+    let mut lexer = Lexer::new(stdin.lock());
     lexer.get_next_token();
 
     loop {
-        eprint!("ready>");
+        write!(&mut outstream_handle, "ready>").unwrap();
         match lexer.current_token() {
             Some(Token::EOF) | None => return,
             Some(Token::Misc(';')) => lexer.get_next_token(),
-            Some(Token::Def) => handle_function_definition(&mut parser, &mut lexer),
-            Some(Token::Extern) => handle_extern(&mut parser, &mut lexer),
-            _ => handle_top_level_expression(&mut parser, &mut lexer),
+            Some(Token::Def) => {
+                handle_function_definition(&mut parser, &mut lexer, &mut outstream_handle)
+            }
+            Some(Token::Extern) => handle_extern(&mut parser, &mut lexer, &mut outstream_handle),
+            _ => handle_top_level_expression(&mut parser, &mut lexer, &mut outstream_handle),
         }
     }
 }
