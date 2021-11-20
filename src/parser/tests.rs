ca99adfe43527e8b06b8f5676cfd233cb095e2fb
Peter Tolsma <pejato@umich.edu>
2021-11-20 17:12:08 -0600

    Add some parse_primary_expr tests

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -109,3 +109,125 @@ fn test_parse_identifier_prefixed_expr_parses_call() {
         _ => assert!(false, "Expected {:#?}", expected_value),
     }
 }
+
+#[test]
+fn test_parse_identifier_prefixed_expr_parsed_call_multiple_args() {
+    let reader = "ident66(30, 60, 90)".as_bytes();
+    let mut parser = Parser::new();
+    let mut consumer = Lexer::new(reader);
+    consumer.get_next_token();
+
+    let result = parser.parse_identifier_prefixed_expr("ident42".into(), &mut consumer);
+
+    let expected_value = Expr {
+        kind: ExprKind::Call {
+            args: vec![
+                Expr {
+                    kind: ExprKind::Number { value: 30.0 },
+                },
+                Expr {
+                    kind: ExprKind::Number { value: 60.0 },
+                },
+                Expr {
+                    kind: ExprKind::Number { value: 90.0 },
+                },
+            ],
+            callee: "ident42".into(),
+        },
+    };
+    match result {
+        Some(expr) if expr == expected_value => (),
+        _ => assert!(false, "Expected {:#?}", expected_value),
+    }
+}
+
+#[test]
+fn test_parse_primary_expr_parses_number() {
+    let reader = "657".as_bytes();
+    let mut parser = Parser::new();
+    let mut lexer = Lexer::new(reader);
+    lexer.get_next_token();
+
+    let result = parser.parse_primary_expr(&mut lexer);
+
+    assert_eq!(
+        result,
+        Expr {
+            kind: ExprKind::Number { value: 657.0 }
+        }
+        .into()
+    );
+}
+
+#[test]
+fn test_parse_primary_expr_parses_ident_prefix_into_variable() {
+    let reader = "suwooooo".as_bytes();
+    let mut parser = Parser::new();
+    let mut lexer = Lexer::new(reader);
+    lexer.get_next_token();
+
+    let result = parser.parse_primary_expr(&mut lexer);
+    assert_eq!(
+        result,
+        Expr {
+            kind: ExprKind::Variable {
+                name: "suwooooo".into()
+            }
+        }
+        .into()
+    );
+}
+
+#[test]
+fn test_parse_primary_expr_parses_ident_prefix_into_call() {
+    let reader = "suwooooo()".as_bytes();
+    let mut parser = Parser::new();
+    let mut lexer = Lexer::new(reader);
+    lexer.get_next_token();
+
+    let result = parser.parse_primary_expr(&mut lexer);
+    assert_eq!(
+        result,
+        Expr {
+            kind: ExprKind::Call {
+                args: vec![],
+                callee: "suwooooo".into()
+            }
+        }
+        .into()
+    );
+}
+
+// #[test]
+// fn test_parse_primary_expr_parses_paren_expr() {
+//     let reader = "(5 + yar())".as_bytes();
+//     let mut parser = Parser::new();
+//     let mut lexer = Lexer::new(reader);
+//     lexer.get_next_token();
+
+//     let result = parser.parse_primary_expr(&mut lexer);
+//     let expected_lhs = Expr {
+//         kind: ExprKind::Number { value: 5.0 },
+//     };
+//     let expected_rhs = Expr {
+//         kind: ExprKind::Call {
+//             callee: "yar".into(),
+//             args: vec![],
+//         },
+//     };
+//     assert_eq!(
+//         result,
+//         Expr {
+//             kind: ExprKind::Binary {
+//                 lhs: expected_lhs.into(),
+//                 rhs: expected_rhs.into(),
+//                 operator: '+'
+//             }
+//         }
+//         .into()
+//     );
+// }
+
+// TODO: Pass write stream to use where we use eprintln! currently
+// #[test]
+// fn test_parse_primary_expr_logs_error() {}
