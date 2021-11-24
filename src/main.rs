be9373976864ef5bf102049f5c8d5065cd28471e
Peter Tolsma <pejato@umich.edu>
2021-11-23 20:26:32 -0600

    Straightforward copy/paste of driver code into a driver struct with associated trait

--- src/main.rs
+++ src/main.rs
@@ -10,40 +10,76 @@ pub mod lexer;
 pub mod parser;
 mod test_utilities;
 
-// TODO: Refactor these into a Driver struct or something
-fn handle_function_definition<T: BufRead>(
-    parser: &mut Parser,
-    lexer: &mut Lexer<T>,
-    output: &mut dyn Write,
-) {
-    if parser.parse_function_definition(lexer).is_some() {
-        write!(output, "Parsed a function definition").unwrap();
-    } else {
-        lexer.get_next_token();
-    }
+trait Drive {
+    fn new() -> Self;
+    fn handle_function_definition<T: BufRead>(
+        &self,
+        parser: &mut Parser,
+        lexer: &mut Lexer<T>,
+        output: &mut dyn Write,
+    );
+    fn handle_extern<T: BufRead>(
+        &self,
+        parser: &mut Parser,
+        lexer: &mut Lexer<T>,
+        output: &mut dyn Write,
+    );
+    fn handle_top_level_expression<T: BufRead>(
+        &self,
+        parser: &mut Parser,
+        lexer: &mut Lexer<T>,
+        output: &mut dyn Write,
+    );
 }
 
-fn handle_extern<T: BufRead>(parser: &mut Parser, lexer: &mut Lexer<T>, output: &mut dyn Write) {
-    if parser.parse_extern(lexer).is_some() {
-        write!(output, "Parsed an extern").unwrap();
-    } else {
-        lexer.get_next_token();
-    }
-}
+struct Driver {}
 
-fn handle_top_level_expression<T: BufRead>(
-    parser: &mut Parser,
-    lexer: &mut Lexer<T>,
-    output: &mut dyn Write,
-) {
-    if parser.parse_top_level_expression(lexer).is_some() {
-        write!(output, "Parsed a top level expression").unwrap();
-    } else {
-        lexer.get_next_token();
+impl Drive for Driver {
+    fn new() -> Self {
+        Driver {}
+    }
+    fn handle_function_definition<T: BufRead>(
+        &self,
+        parser: &mut Parser,
+        lexer: &mut Lexer<T>,
+        output: &mut dyn Write,
+    ) {
+        if parser.parse_function_definition(lexer).is_some() {
+            write!(output, "Parsed a function definition").unwrap();
+        } else {
+            lexer.get_next_token();
+        }
+    }
+
+    fn handle_extern<T: BufRead>(
+        &self,
+        parser: &mut Parser,
+        lexer: &mut Lexer<T>,
+        output: &mut dyn Write,
+    ) {
+        if parser.parse_extern(lexer).is_some() {
+            write!(output, "Parsed an extern").unwrap();
+        } else {
+            lexer.get_next_token();
+        }
+    }
+
+    fn handle_top_level_expression<T: BufRead>(
+        &self,
+        parser: &mut Parser,
+        lexer: &mut Lexer<T>,
+        output: &mut dyn Write,
+    ) {
+        if parser.parse_top_level_expression(lexer).is_some() {
+            write!(output, "Parsed a top level expression").unwrap();
+        } else {
+            lexer.get_next_token();
+        }
     }
 }
 
 fn main() {
+    let driver = Driver::new();
     let mut parser = Parser::new();
     let stdin = stdin();
     let stdout = stdout();
@@ -57,10 +93,12 @@ fn main() {
             Some(Token::EOF) | None => return,
             Some(Token::Misc(';')) => lexer.get_next_token(),
             Some(Token::Def) => {
-                handle_function_definition(&mut parser, &mut lexer, &mut stdout.lock())
+                driver.handle_function_definition(&mut parser, &mut lexer, &mut stdout.lock())
             }
-            Some(Token::Extern) => handle_extern(&mut parser, &mut lexer, &mut stdout.lock()),
-            _ => handle_top_level_expression(&mut parser, &mut lexer, &mut stdout.lock()),
+            Some(Token::Extern) => {
+                driver.handle_extern(&mut parser, &mut lexer, &mut stdout.lock())
+            }
+            _ => driver.handle_top_level_expression(&mut parser, &mut lexer, &mut stdout.lock()),
         }
     }
 }
