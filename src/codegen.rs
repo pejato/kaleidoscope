dc282cecb2e250cd4f4ad7587fe543f6364039e6
Peter Tolsma <pejato@umich.edu>
2021-11-27 20:25:24 -0500

    Figuring out llvm c api

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,9 +1,7 @@
-use llvm_sys::core::LLVMConstReal;
-use llvm_sys::core::LLVMConstReal;
-use llvm_sys::core::LLVMContextCreate;
+use llvm_sys::core::{LLVMConstReal, LLVMDoubleType};
 use llvm_sys::LLVMContext;
 
 unsafe fn stuff() {
-    let context = LLVMContextCreate();
-    let val = LLVMConstReal(context, 64.0);
+    let mut double_type = LLVMDoubleType();
+    let val = LLVMConstReal(double_type, 64.0);
 }
