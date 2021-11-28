d2861bcbe603b75e153d2ab8bf271fa8b11f6582
Peter Tolsma <pejato@umich.edu>
2021-11-28 14:52:35 -0500

    Add builder/module construction and disposal logic to KaleidoscopeContext

--- src/kaleidoscope_context.rs
+++ src/kaleidoscope_context.rs
@@ -1,14 +1,38 @@
-use llvm_sys::prelude::LLVMValueRef;
-use std::collections::HashMap;
+use llvm_sys::{
+    core::{LLVMCreateBuilder, LLVMDisposeBuilder, LLVMDisposeModule, LLVMModuleCreateWithName},
+    prelude::{LLVMBuilderRef, LLVMModuleRef, LLVMValueRef},
+};
+use std::{collections::HashMap, ffi::CString};
 
 pub struct KaleidoscopeContext {
     pub named_values: HashMap<String, LLVMValueRef>,
+    pub module: LLVMModuleRef,
+    pub builder: LLVMBuilderRef,
+
+    _module_name: CString,
 }
 
 impl KaleidoscopeContext {
     pub fn new() -> Self {
-        Self {
-            named_values: HashMap::new(),
+        unsafe {
+            let _module_name = CString::new("my module").expect("CString::new failed");
+            let _module_name_raw_ptr = _module_name.as_ptr();
+            Self {
+                named_values: HashMap::new(),
+                module: LLVMModuleCreateWithName(_module_name_raw_ptr),
+                // Note: This implicitly uses the global context
+                builder: LLVMCreateBuilder(),
+                _module_name,
+            }
+        }
+    }
+}
+
+impl Drop for KaleidoscopeContext {
+    fn drop(&mut self) {
+        unsafe {
+            LLVMDisposeBuilder(self.builder);
+            LLVMDisposeModule(self.module);
         }
     }
 }
