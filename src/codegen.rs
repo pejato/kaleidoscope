86f155a86d4336c6c08d0ff1743d72bed012d39b
Peter Tolsma <pejato@umich.edu>
2021-11-28 11:14:42 -0500

    Add a CodeGen trait, stub out impl for Expr, add codegen_number logic

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,7 +1,47 @@
 use llvm_sys::core::{LLVMConstReal, LLVMDoubleType};
-use llvm_sys::LLVMContext;
+use llvm_sys::prelude::*;
 
-unsafe fn stuff() {
-    let mut double_type = LLVMDoubleType();
-    let val = LLVMConstReal(double_type, 64.0);
+use crate::ast::{Expr, ExprKind};
+
+pub trait CodeGen {
+    fn codegen(&self) -> LLVMValueRef;
+}
+
+impl CodeGen for Expr {
+    fn codegen(&self) -> LLVMValueRef {
+        match self.kind {
+            ExprKind::Number(num) => self.codegen_number(num),
+            ExprKind::Variable { .. } => self.codegen_variable(),
+            ExprKind::Binary { .. } => self.codegen_binary(),
+            ExprKind::Call { .. } => self.codegen_call(),
+            ExprKind::Prototype { .. } => self.codegen_prototype(),
+            ExprKind::Function { .. } => self.codegen_function(),
+        }
+    }
+}
+
+impl Expr {
+    fn codegen_number(&self, num: f64) -> LLVMValueRef {
+        unsafe { return LLVMConstReal(LLVMDoubleType(), num) }
+    }
+
+    fn codegen_variable(&self) -> LLVMValueRef {
+        todo!()
+    }
+
+    fn codegen_binary(&self) -> LLVMValueRef {
+        todo!()
+    }
+
+    fn codegen_call(&self) -> LLVMValueRef {
+        todo!()
+    }
+
+    fn codegen_prototype(&self) -> LLVMValueRef {
+        todo!()
+    }
+
+    fn codegen_function(&self) -> LLVMValueRef {
+        todo!()
+    }
 }
