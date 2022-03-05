bbc44c743518e0aa06ca317d79e8676ce9bcefec
pejato <pejato@umich.edu>
2022-03-05 12:31:44 -0600

    Revert ref cell

--- src/driver.rs
+++ src/driver.rs
@@ -180,12 +180,11 @@ impl Driver<'_> {
                 let engine = self
                     .codegen
                     .module
-                    .borrow()
                     .create_jit_execution_engine(OptimizationLevel::Aggressive)
                     .unwrap();
 
                 defer!(
-                    engine.remove_module(&self.codegen.module.borrow()).unwrap();
+                    engine.remove_module(&self.codegen.module).unwrap();
                 );
 
                 let fun = unsafe {
@@ -236,7 +235,7 @@ impl Driver<'_> {
         if !self.options.print_ir {
             return Ok(());
         }
-        let llvm_string = self.codegen.module.borrow().print_to_string();
+        let llvm_string = self.codegen.module.print_to_string();
         let as_str = llvm_string
             .to_str()
             .ok()
